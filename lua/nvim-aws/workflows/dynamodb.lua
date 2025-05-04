125893a28a21975fdf52763a387bb8fa1e2184d4
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-05-04 14:17:37 -0700

    feat: adding basic flow for dynamodb service

---
+++ lua/nvim-aws/workflows/dynamodb.lua
@@ -0,0 +1,28 @@
+local dynamodb = require("nvim-aws.autogen_wrappers.dynamodb")
+local fzf = require("fzf-lua")
+local log = require("utilities.log")
+
+local M = {}
+
+--- Displays a list of S3 buckets and allows the user to manage them
+function M.manage_tables()
+	log.debug("Entering manage_tables function")
+	local result = dynamodb.list_tables({})
+	if not result.success then
+		log.error("Error: " .. (result.error or "Failed to fetch tables"))
+		return
+	end
+
+	local tables = result.data.TableNames
+
+	fzf.fzf_exec(tables, {
+		prompt = "Select DynamoDB Table> ",
+		actions = {
+			["default"] = function(selected)
+				print("Selected table: " .. selected[1])
+			end,
+		},
+	})
+end
+
+return M
