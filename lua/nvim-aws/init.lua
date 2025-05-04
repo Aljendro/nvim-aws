9ea3f24572f711c3915cf7225a13d92b1ec97b15
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-05-04 15:10:04 -0700

    feat: using an aws profile rather than default values for config

--- lua/nvim-aws/init.lua
+++ lua/nvim-aws/init.lua
@@ -6,10 +6,7 @@ M.setup = function(opts)
 	-- Default configuration
 	local config = {
 		aws = {
-      -- NOTE: Required to default to us-east-1 for localstack testing
-			aws_region = opts.aws and opts.aws.aws_region or "us-east-1",
-			aws_profile = opts.aws and opts.aws.aws_profile or "default",
-			aws_endpoint_url = opts.aws and opts.aws.aws_endpoint_url or "http://localhost:4566",
+			aws_profile = opts.aws and opts.aws.aws_profile or "nvim-aws",
 		},
 	}
 
