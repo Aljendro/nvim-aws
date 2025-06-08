08ff5c18a4c0a43244bec724e3bd15a76e349095
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-06-07 23:39:53 -0700

    style: add comment for creating a new tab in result buffer generation function

--- lua/nvim-aws/workflows/common.lua
+++ lua/nvim-aws/workflows/common.lua
@@ -67,11 +67,11 @@ end
 --- Generate a buffer to output the results of running a aws command
 --- @return integer
 function M.gen_result_buffer()
+  -- create a new tab for the result ai!
 	local result_buf = vim.api.nvim_create_buf(true, true)
 	local uuid = default_utility.generate_uuid()
 	vim.api.nvim_buf_set_name(result_buf, "aws-result-" .. uuid)
 	vim.api.nvim_set_option_value("filetype", "json", { buf = result_buf })
-
 	vim.api.nvim_win_set_buf(0, result_buf)
 
 	return result_buf
