70932b5b3c507d8ba19df4ac91fc8bf3425e6b3d
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-06-07 18:43:20 -0700

    test: add extend query functionality tests for log buffer prepend and append scenarios

--- lua/spec/nvim-aws/workflows/logs/utility_spec.lua
+++ lua/spec/nvim-aws/workflows/logs/utility_spec.lua
@@ -37,8 +37,8 @@ describe("nvim-aws.workflows.logs.utility", function()
 			return result_buf
 		end)
 		-- Stub AWS logs API
-    local firstLogTS = 1749327258558
-    local lastLogTS = 1749327263373
+		local firstLogTS = 1749327258558
+		local lastLogTS = 1749327263373
 		local fake_events = {
 			{ timestamp = firstLogTS, message = "event1" },
 			{ timestamp = lastLogTS, message = "event2" },
@@ -76,7 +76,10 @@ describe("nvim-aws.workflows.logs.utility", function()
 		callback({ buf = form_buf })
 		-- Verify results
 		local result_lines = vim.api.nvim_buf_get_lines(result_buf, 0, -1, false)
-		assert.equals(string.format("(<<< startTime: %d, endTime: %d)", firstLogTS - 600000, firstLogTS - 1), result_lines[2])
+		assert.equals(
+			string.format("(<<< startTime: %d, endTime: %d)", firstLogTS - 600000, firstLogTS - 1),
+			result_lines[2]
+		)
 		assert.equals("event1", result_lines[3]:match("%) (.+)"))
 		assert.equals(
 			string.format("(>>> startTime: %d, endTime: %d)", lastLogTS + 1, lastLogTS + 600000),
@@ -85,5 +88,152 @@ describe("nvim-aws.workflows.logs.utility", function()
 		-- Revert stubs
 		st_logs:revert()
 		workflows_common.gen_result_buffer:revert()
+	end)
+
+	-- Can you check these tests and make sure they are working properly ai!
+	-- Scenario 1:
+	-- pressing the extend keybinding on (<<< startTime: <unix ts A>, endTime: <unix ts B>)
+	-- we want the the code to query between those two timestamps and replace it with another (<<< startTime: <unix ms>, endTime: <unix ms>) at the top of the file and the contents of the logs from the query
+	-- the new startTime will be 10 minutes before the the old startTime <unix ts A> and the new endTime will be 1 ms before the old endTime <unix ts B>
+	-- IF the query results in a nextToken, it will produce a (>>> startTime: <unix ts>, endTime: <unix ts>) after the new logs just above the previous contents of the existing logs in the buffer. The startTime and endTime of this (>>> startTime: <unix ts>, endTime: <unix ts>) will not exceed the original <unix ts A>
+	-- Scenario 2:
+	-- pressing the extend keybinding on (>>> startTime: <unix ts C>, endTime: <unix ts D>)
+	-- we want the the code to query between those two timestamps and produce another (>>> startTime: <unix ms>, endTime: <unix ms>) at the bottom of the file
+	-- the new startTime should be 1 ms after the previous endTime <unix ts D> and the new endTime should be 10 minutes after the endTime <unix ts D>
+	it("should test the extend query functionality - Scenario 1 (prepend)", function()
+		local workflows_common = require("nvim-aws.workflows.common")
+		local logs_wrapper = require("nvim-aws.autogen_wrappers.logs")
+		local result_buf = vim.api.nvim_create_buf(false, true)
+
+		-- Setup initial buffer content with timestamp markers
+		local startTimeA = 1649327258000 -- Example timestamp A
+		local endTimeB = 1649327558000 -- Example timestamp B
+		local initialLines = {
+			string.format("(<<< startTime: %d, endTime: %d)", startTimeA, endTimeB),
+			"Existing log content line 1",
+			"Existing log content line 2",
+		}
+		vim.api.nvim_buf_set_lines(result_buf, 0, -1, false, initialLines)
+
+		-- Position cursor on the marker line
+		stub(vim.api, "nvim_win_get_cursor", function()
+			return { 1, 0 }
+		end)
+
+		-- Setup fake events and nextToken for the query response
+		local eventsTimestamps = {
+			startTimeA - 300000, -- 5 minutes before startTimeA
+			startTimeA - 120000, -- 2 minutes before startTimeA
+		}
+		local fake_events = {
+			{ timestamp = eventsTimestamps[1], message = "Prepended event 1" },
+			{ timestamp = eventsTimestamps[2], message = "Prepended event 2" },
+		}
+
+		-- Stub the API call with nextToken to test that scenario
+		stub(logs_wrapper, "filter_log_events", function(_)
+			return {
+				success = true,
+				data = {
+					events = fake_events,
+					nextToken = "fake-next-token",
+				},
+			}
+		end)
+
+		-- Call the function with base params
+		local params = { logGroupName = "test-group" }
+		utility._extend_query(result_buf, params)
+
+		-- Verify the results
+		local result_lines = vim.api.nvim_buf_get_lines(result_buf, 0, -1, false)
+
+		-- Should have the new <<< marker with timestamps 10 minutes before
+		local expected_new_start = eventsTimestamps[1] - 600000
+		local expected_new_end = eventsTimestamps[1] - 1
+		assert.is_true(result_lines[1]:find("<<<") > 0, "First line should have <<< marker")
+		assert.equals(
+			string.format("(<<< startTime: %d, endTime: %d)", expected_new_start, expected_new_end),
+			result_lines[1]
+		)
+
+		-- Should have the event content
+		assert.equals("(", result_lines[2]:sub(1, 1), "Line 2 should begin with timestamp marker")
+		assert.is_true(result_lines[2]:find("Prepended event 1") > 0, "Line 2 should contain event 1")
+		assert.equals("(", result_lines[3]:sub(1, 1), "Line 3 should begin with timestamp marker")
+		assert.is_true(result_lines[3]:find("Prepended event 2") > 0, "Line 3 should contain event 2")
+
+		-- Should have >>> marker since nextToken was present
+		local append_start = eventsTimestamps[2] + 1
+		local append_end = eventsTimestamps[2] + 600000
+		assert.equals(string.format("(>>> startTime: %d, endTime: %d)", append_start, append_end), result_lines[4])
+
+		-- Clean up stubs
+		vim.api.nvim_win_get_cursor:revert()
+		logs_wrapper.filter_log_events:revert()
+	end)
+
+	it("should test the extend query functionality - Scenario 2 (append)", function()
+		local workflows_common = require("nvim-aws.workflows.common")
+		local logs_wrapper = require("nvim-aws.autogen_wrappers.logs")
+		local result_buf = vim.api.nvim_create_buf(false, true)
+
+		-- Setup initial buffer content with timestamp markers
+		local startTimeC = 1649327558000 -- Example timestamp C
+		local endTimeD = 1649327858000 -- Example timestamp D
+		local initialLines = {
+			"Existing log content line 1",
+			"Existing log content line 2",
+			string.format("(>>> startTime: %d, endTime: %d)", startTimeC, endTimeD),
+		}
+		vim.api.nvim_buf_set_lines(result_buf, 0, -1, false, initialLines)
+
+		-- Position cursor on the marker line (line 3)
+		stub(vim.api, "nvim_win_get_cursor", function()
+			return { 3, 0 }
+		end)
+
+		-- Setup fake events for the query response
+		local eventsTimestamps = {
+			endTimeD + 60000, -- 1 minute after endTimeD
+			endTimeD + 300000, -- 5 minutes after endTimeD
+		}
+		local fake_events = {
+			{ timestamp = eventsTimestamps[1], message = "Appended event 1" },
+			{ timestamp = eventsTimestamps[2], message = "Appended event 2" },
+		}
+
+		stub(logs_wrapper, "filter_log_events", function(_)
+			return { success = true, data = { events = fake_events } }
+		end)
+
+		-- Call the function with base params
+		local params = { logGroupName = "test-group" }
+		utility._extend_query(result_buf, params)
+
+		-- Verify the results
+		local result_lines = vim.api.nvim_buf_get_lines(result_buf, 0, -1, false)
+
+		-- Original content should remain
+		assert.equals("Existing log content line 1", result_lines[1])
+		assert.equals("Existing log content line 2", result_lines[2])
+
+		-- Should have the event content
+		assert.equals("(", result_lines[3]:sub(1, 1), "Line 3 should begin with timestamp marker")
+		assert.is_true(result_lines[3]:find("Appended event 1") > 0, "Line 3 should contain event 1")
+		assert.equals("(", result_lines[4]:sub(1, 1), "Line 4 should begin with timestamp marker")
+		assert.is_true(result_lines[4]:find("Appended event 2") > 0, "Line 4 should contain event 2")
+
+		-- Should have new >>> marker with appropriate timestamps
+		local expected_new_start = eventsTimestamps[2] + 1
+		local expected_new_end = eventsTimestamps[2] + 600000
+		assert.equals(
+			string.format("(>>> startTime: %d, endTime: %d)", expected_new_start, expected_new_end),
+			result_lines[5]
+		)
+
+		-- Clean up stubs
+		vim.api.nvim_win_get_cursor:revert()
+		logs_wrapper.filter_log_events:revert()
 	end)
 end)
